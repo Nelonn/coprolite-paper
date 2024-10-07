98c724e7f126b14d7379b97879a276656c67d230
Nelonn <42481486+Nelonn@users.noreply.github.com>
2024-10-07 15:10:44 +0300

    Improved mixin for plugins command

--- coprolite-paper/src/main/java/me/nelonn/coprolite/paper/mixin/MixinPaperPluginsCommand.java
+++ coprolite-paper/src/main/java/me/nelonn/coprolite/paper/mixin/MixinPaperPluginsCommand.java
@@ -18,12 +18,6 @@ package me.nelonn.coprolite.paper.mixin;
 
 import com.google.common.collect.Lists;
 import io.papermc.paper.command.PaperPluginsCommand;
-import io.papermc.paper.plugin.configuration.PluginMeta;
-import io.papermc.paper.plugin.entrypoint.Entrypoint;
-import io.papermc.paper.plugin.entrypoint.LaunchEntryPointHandler;
-import io.papermc.paper.plugin.provider.PluginProvider;
-import io.papermc.paper.plugin.provider.type.paper.PaperPluginParent;
-import io.papermc.paper.plugin.provider.type.spigot.SpigotPluginProvider;
 import me.nelonn.coprolite.api.CoproliteLoader;
 import me.nelonn.coprolite.api.PluginContainer;
 import me.nelonn.coprolite.api.PluginMetadata;
@@ -31,11 +25,15 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,21 +41,15 @@ import java.util.TreeMap;
 
 @Mixin(PaperPluginsCommand.class)
 public abstract class MixinPaperPluginsCommand {
-    @Unique
-    private static final Component COPROLITE_HEADER = Component.text("Coprolite Plugins:", TextColor.color(153, 51, 255));
 
-    @Shadow @Final private static Component PAPER_HEADER;
-    @Shadow @Final private static Component BUKKIT_HEADER;
     @Shadow @Final private static Component PLUGIN_TICK;
     @Shadow @Final private static Component PLUGIN_TICK_EMPTY;
 
-    @Shadow
-    private static <T> List<Component> formatProviders(TreeMap<String, PluginProvider<T>> plugins) {
-        return null;
-    }
+    @Unique
+    private static final Component COPROLITE_HEADER = Component.text("Coprolite Plugins:", TextColor.color(153, 51, 255));
 
     @Unique
-    private static List<Component> formatPlugins(TreeMap<String, PluginMetadata> plugins) {
+    private static List<Component> coprolite_paper$formatPlugins(TreeMap<String, PluginMetadata> plugins) {
         List<Component> components = new ArrayList<>(plugins.size());
         for (PluginMetadata metadata : plugins.values()) {
             components.add(Component.text(metadata.getName() != null ? metadata.getName() : metadata.getId(),
@@ -89,65 +81,23 @@ public abstract class MixinPaperPluginsCommand {
         return formattedSublists;
     }
 
-    /**
-     * @author Nelonn
-     * @reason show coprolite paper plugins in console
-     */
-    @Overwrite
-    public boolean execute(@NotNull CommandSender sender, @NotNull String currentAlias, @NotNull String[] args) {
-        if (!((Command) (Object) this).testPermission(sender)) return true;
+    @Redirect(method = "execute", at = @At(value = "INVOKE", target = "Ljava/lang/String;formatted([Ljava/lang/Object;)Ljava/lang/String;"))
+    private String coprolite_paper$addCount(String instance, Object[] args) {
+        if (args.length == 0 || !(args[0] instanceof Integer integer)) return instance.formatted(args);
+        return instance.formatted(integer + CoproliteLoader.getInstance().getAllPlugins().size());
+    }
 
-        // Coprolite start
+    @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lorg/bukkit/command/CommandSender;sendMessage(Lnet/kyori/adventure/text/Component;)V", ordinal = 1))
+    private void coprolite_paper$sendPlugins(CommandSender sender, String currentAlias, String[] args, CallbackInfoReturnable<Boolean> cir) {
         TreeMap<String, PluginMetadata> coprolitePlugins = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         for (PluginContainer plugin : CoproliteLoader.getInstance().getAllPlugins()) {
             coprolitePlugins.put(plugin.getMetadata().getName(), plugin.getMetadata());
         }
-        // Coprolite end
-        TreeMap<String, PluginProvider<JavaPlugin>> paperPlugins = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-        TreeMap<String, PluginProvider<JavaPlugin>> spigotPlugins = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-
-
-        for (PluginProvider<JavaPlugin> provider : LaunchEntryPointHandler.INSTANCE.get(Entrypoint.PLUGIN).getRegisteredProviders()) {
-            PluginMeta configuration = provider.getMeta();
-
-            if (provider instanceof SpigotPluginProvider) {
-                spigotPlugins.put(configuration.getDisplayName(), provider);
-            } else if (provider instanceof PaperPluginParent.PaperServerPluginProvider) {
-                paperPlugins.put(configuration.getDisplayName(), provider);
-            }
-        }
-
-        Component infoMessage = Component.text("Server Plugins (%s):".formatted(coprolitePlugins.size() + paperPlugins.size() + spigotPlugins.size()), NamedTextColor.WHITE); // Coprolite
-        //.append(INFO_ICON_START.hoverEvent(SERVER_PLUGIN_INFO)); TODO: Add docs
-
-        sender.sendMessage(infoMessage);
-
-        // Coprolite start
         if (!coprolitePlugins.isEmpty()) {
             sender.sendMessage(COPROLITE_HEADER);
         }
-
-        for (Component component : formatPlugins(coprolitePlugins)) {
+        for (Component component : coprolite_paper$formatPlugins(coprolitePlugins)) {
             sender.sendMessage(component);
         }
-
-        // Coprolite end
-        if (!paperPlugins.isEmpty()) {
-            sender.sendMessage(PAPER_HEADER);
-        }
-
-        for (Component component : formatProviders(paperPlugins)) {
-            sender.sendMessage(component);
-        }
-
-        if (!spigotPlugins.isEmpty()) {
-            sender.sendMessage(BUKKIT_HEADER);
-        }
-
-        for (Component component : formatProviders(spigotPlugins)) {
-            sender.sendMessage(component);
-        }
-
-        return true;
     }
 }
